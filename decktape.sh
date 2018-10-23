202839f4fa35885a47dd35d128fbe907017e1862
liao <liaomovie2@gmail.com>
2018-10-23 17:03:37 +0800

    abstractize decktape

--- decktape.sh
+++ decktape.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-file=${1}
-pause=${2}
+pause=${1}
 
-node_modules/.bin/decktape --pause=${pause} --size=1024x768 --chrome-arg=--allow-file-access-from-files remark ${file}/index.html ${file}/${file}.pdf
+# Read the newest slide if no input
+p=$(head -n 1 list.txt)
+file=$(echo "$p" | cut -d ',' -f 2)
+
+node_modules/.bin/decktape --pause=$pause --size=1024x768 --chrome-arg=--allow-file-access-from-files remark ${file}/index.html ${file}/${file}.pdf
