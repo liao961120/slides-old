4ad3eb9c20b37d6d17b54a420fa8b4eb0eec48de
liao <liaomovie2@gmail.com>
2018-12-27 20:28:19 +0800

    docker failed

--- build-slide.sh
+++ build-slide.sh
@@ -18,9 +18,9 @@ while read p; do
         [[ -d ${file} ]] && cp -r archive/${file}/* ${file}/
     else
         Rscript -e "rmarkdown::render(\"${file}/index.Rmd\")"
-        #node_modules/.bin/decktape --pause=$pause --size=1024x768 \
-        #    --chrome-arg=--allow-file-access-from-files \
-        #    remark ${file}/index.html ${file}/${file}.pdf
+        node_modules/.bin/decktape --pause=$pause --size=1024x768 \
+            --chrome-arg=--allow-file-access-from-files \
+            remark ${file}/index.html ${file}/${file}.pdf
     fi
 done < list.txt
 
@@ -34,12 +34,9 @@ file=$(head -n 1 list.txt | cut -d ',' -f 2)
 Rscript -e "rmarkdown::render(\"${file}/index.Rmd\")"
 
 echo 'decktape: start printing PDF'
-sudo docker run --rm -it --privileged --net=host -v `pwd`:/slides:rw \
-    astefanutti/decktape --pause=$pause remark \
-    http://localhost:8000/${file}/ ${file}/index.pdf
-#node_modules/.bin/decktape --pause=$pause --size=1024x768 \
-#    --chrome-arg=--allow-file-access-from-files remark \
-#    ${file}/index.html ${file}/${file}.pdf
+node_modules/.bin/decktape --pause=$pause --size=1024x768 \
+    --chrome-arg=--allow-file-access-from-files remark \
+    ${file}/index.html ${file}/${file}.pdf
 
 #docker run --rm -it --privileged -v `pwd`:/slides -v ~:/home/user astefanutti/decktape \
 #    --pause=${pause} --size=1024x768 \
