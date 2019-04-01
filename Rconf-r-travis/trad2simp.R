43d044bc3427ce5f23b2ee7b8997320bc8ed06b1
liao <liaomovie2@gmail.com>
2019-04-01 15:26:19 +0800

    up

--- Rconf-r-travis/trad2simp.R
+++ Rconf-r-travis/trad2simp.R
@@ -10,11 +10,11 @@ writeLines(simp, 'index_simp.Rmd')
 ## Copy to slide folder
 
 source_files <- list.files(full.names = TRUE)
-trad_source_idx <- which(grepl('(Rmd)|(html)$', source_files))
+trad_source_idx <- which(grepl('(Rmd)|(html)|(_files)$', source_files))
 source_files <- source_files[-trad_source_idx]
 
 file.copy(from = source_files, to = '~/slides/Rconf-r-travis', 
           recursive = TRUE)
 
 file.copy(from = './index_simp.Rmd', 
-          to = '~/slides/Rconf-r-travis/index.Rmd')
+          to = '~/slides/Rconf-r-travis/index.Rmd', overwrite = TRUE)
